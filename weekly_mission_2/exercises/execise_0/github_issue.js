2371936b94480456f3c73efa76e31e543f457513
joslo2345 <soulsilversx@gmail.com>
2022-04-13 02:17:41 -0500

    se agrega ejercicios de semana 2

---
+++ weekly_mission_2/exercises/execise_0/github_issue.js
@@ -0,0 +1,32 @@
+const issue = {
+    title: 'problema en el backend',
+    repositoryNameAssociated: 'playbook',
+    status : 'open',
+    numberOfComments: '3',
+    labels: ['nodejs','javascript','backend'],
+    author: 'joslo2345',
+    dateCreated: new Date(),
+    lastUpdated: new Date(),
+    getTitleAndAuthor: function(){
+        console.log(`Title: ${this.title} \nAuthor: ${this.author} `)
+    },
+    getGeneralInfo: function(){
+        var generalInfo = `
+        Title: ${this.title} \n
+        Repository associated: ${this.repositoryNameAssociated} \n
+        Number of comments: ${this.numberOfComments} \n
+        Labels: ${this.labels} \n
+        Author: ${this.author} \n
+        Date of creation: ${this.dateCreated} \n
+        Last updated: ${this.lastUpdated}
+        `;
+        return generalInfo;
+    }
+}
+console.log('Title of the issue' + ' "' + issue.title + '" ');
+console.log('Author of the issue ' + issue.author);
+console.log('\n\n')
+issue.getTitleAndAuthor()
+console.log("General info: \n", issue.getGeneralInfo());
+
+
---
+++ weekly_mission_2/exercises/execise_0/github_pullrequest.js
@@ -0,0 +1,17 @@
+const pullRequest = {
+    title: 'cambios al backend',
+    branchName: 'modulo de mediciones',
+    author: 'joslo2345',
+    dateCreated: new Date(),
+    status: 'open',
+    repositoryNameAssociated: 'prueba de cohete',
+    getStatus: function(){
+        return this.status;
+    },
+    getTitleAndAutor: function(){
+        return `Title: ${this.title} \nAuthor: ${this.author}`
+    }
+}
+
+console.log(pullRequest.getTitleAndAutor())
+console.log(`Status: ${pullRequest.getStatus()}`)
